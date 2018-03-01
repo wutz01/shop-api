07eb1e81b034949b77854ee4c183f23309e600a5
John Perez <john.perez@kodakit.com>
2018-03-02 00:33:38 +0800

    Fixed seeder

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -15,71 +15,73 @@ class ProductsSeeder extends Seeder
     public function run()
     {
         $cat1 = new Category;
-        $cat1->name = 'Electronic Device';
-        $cat1->description = 'Devices';
+        $cat1->name = 'Multiparameter Photometers';
+        $cat1->description = '';
         $cat1->save();
 
         $cat2 = new Category;
-        $cat2->name = 'Sports & Travel';
-        $cat2->description = 'Wearable and sports';
+        $cat2->name = 'Single Parameter Photometers';
+        $cat2->description = '';
         $cat2->save();
 
         $cat3 = new Category;
-        $cat3->name = 'Audio Devices';
-        $cat3->description = 'Audio';
+        $cat3->name = 'Sensor Technology';
+        $cat3->description = '';
         $cat3->save();
 
         $cat4 = new Category;
-        $cat4->name = 'Video Devices';
-        $cat4->description = 'Video';
+        $cat4->name = 'Electrochemical Meters';
+        $cat4->description = '';
         $cat4->save();
 
         $sup1 = new Supplier;
-        $sup1->name = 'MowBow';
+        $sup1->name = 'TBS';
         $sup1->address = 'Makati';
         $sup1->currency = 'PHP';
         $sup1->save();
 
-        $sup2 = new Supplier;
-        $sup2->name = 'BowWow';
-        $sup2->address = 'Japan';
-        $sup2->currency = 'Yen';
-        $sup2->save();
+        $forCat1 = ['Photometer 8000', 'Photometer 7500', 'Photometer 7100'];
+        foreach ($forCat1 as $key => $value) {
+          $product = new Product;
+          $product->name = $value;
+          $product->description = '';
+          $product->price = 0.00;
+          $product->supplierId = $sup1->id;
+          $product->save();
+          $product->categories()->sync($cat1->id);
+        }
 
-        $sup3 = new Supplier;
-        $sup3->name = 'Glooble';
-        $sup3->address = 'Florida, USA';
-        $sup3->currency = 'USD';
-        $sup3->save();
+        $forCat2 = ['Compact Turbimeter', 'Compact Chlorometer', 'Compact Chlorometer Duo', 'Compact ClO2+ Meter', 'Compact Ozone Meter', 'Compact Ammonia + Meter', 'Compact Ammonia Duo Meter'];
+        foreach ($forCat2 as $key => $value) {
+          $product = new Product;
+          $product->name = $value;
+          $product->description = '';
+          $product->price = 0.00;
+          $product->supplierId = $sup1->id;
+          $product->save();
+          $product->categories()->sync($cat2->id);
+        }
 
-        $sup4 = new Supplier;
-        $sup4->name = 'White Gadgeteer';
-        $sup4->address = 'United Kingdom';
-        $sup4->currency = 'GBP';
-        $sup4->save();
+        $forCat3 = ['ChlordioX Plus', 'ChlordioXense', 'ChloroSense', 'Scanning Analyzer'];
+        foreach ($forCat3 as $key => $value) {
+          $product = new Product;
+          $product->name = $value;
+          $product->description = '';
+          $product->price = 0.00;
+          $product->supplierId = $sup1->id;
+          $product->save();
+          $product->categories()->sync($cat3->id);
+        }
 
-        $pro1 = new Product;
-        $pro1->name = 'Speaker';
-        $pro1->description = '7.1 Surround Sound';
-        $pro1->price = 500.00;
-        $pro1->supplierId = $sup1->id;
-        $pro1->save();
-        $pro1->categories()->sync([$cat1->id, $cat3->id]);
-
-        $pro2 = new Product;
-        $pro2->name = 'HDMI Cable';
-        $pro2->description = '3 Meters cable';
-        $pro2->price = 35.00;
-        $pro2->supplierId = $sup3->id;
-        $pro2->save();
-        $pro2->categories()->sync([$cat1->id, $cat3->id, $cat4->id]);
-
-        $pro3 = new Product;
-        $pro3->name = 'Water Jog';
-        $pro3->description = 'Can contain 2 liters of liquid';
-        $pro3->price = 30.00;
-        $pro3->supplierId = $sup4->id;
-        $pro3->save();
-        $pro3->categories()->sync($cat2->id);
+        $forCat4 = ['pH', 'Conductivity / TDS', 'Dissolved Oxygen', 'Multiparameter'];
+        foreach ($forCat4 as $key => $value) {
+          $product = new Product;
+          $product->name = $value;
+          $product->description = '';
+          $product->price = 0.00;
+          $product->supplierId = $sup1->id;
+          $product->save();
+          $product->categories()->sync($cat4->id);
+        }
     }
 }
